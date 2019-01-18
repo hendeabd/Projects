d377e29c61d6a718a1db1a4e9d62aa9dee3df577
unknown <hendeabd000@warren.k12.in.us>
2019-01-18 13:29:20 -0500

    fini

--- MadLibs/madlibs.js
+++ MadLibs/madlibs.js
@@ -34,5 +34,5 @@ function showInfo(){
     var message = "Dear Mr. and Mrs." + sillyName + sillyWord + ", Will you let me" + verb + "your" + noun + "? Ever scince I have laid" + bodyPart + "on" + femaleName + ", I have" + verb_ed + ",adly in love with her. I wish that she will be the" + noun + "of my" + plural_noun + "and that someday we will" + verb + "happily ever after. I have a" + noun + "as a/an" + occupation + "that pays" + number + "each month. I promise to" + verb + + femaleName + "with kindness and respect. Sincerely," + sillyWord + sillyName + ".";
 
     $("#output").text(message);
-    $("#output").text(message).show().animate({"top":"0px"});
+    $("#output").text(message).show()
 }
