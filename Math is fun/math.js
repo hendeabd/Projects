f23b0410a6eeaaa6942990a134684980550301ae
unknown <hendeabd000@warren.k12.in.us>
2019-01-28 13:44:09 -0500

    trash

---
+++ Math is fun/math.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    
+    $(time).click(function(){
+        time();
+    });
+
+});
+
+var time = function(){
+
+    var daysRemaining;
+    var minutesRemaining;
+    var minutes;
+    var years;
+    var days;
+
+    var input = $("#label1").val();
+
+}
+
