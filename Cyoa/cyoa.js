86050a7d856514b6b0932fcc352b68d18d430c43
unknown <hendeabd000@warren.k12.in.us>
2019-02-19 13:43:37 -0500

    oof

---
+++ Cyoa/cyoa.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    updateNarrative(N1)
+    $("footer").html(createButton("button1", O1) + createButton("button2", O2) + createButton("button3", "not used"));
+    $("button3").hide();
+})
+
+var N1 = "The boy went back to his school after dark to get his..."
+var N2 = "While walking down a hall he started to feel like he was being followed by ..."
+var N3 = "After getting his wallet he was suddenly approached by ..."
+var N4 = ""
+var N5 = ""
+var N6 = ""
+var N7 = ""
+var N8 = ""
+var N9 = ""
+var N10 = ""
