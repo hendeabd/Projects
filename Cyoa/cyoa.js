34a50bd45f480fc3c06ffb7286cb95d52227b1ad
unknown <hendeabd000@warren.k12.in.us>
2019-02-22 13:57:06 -0500

    oof2

--- Cyoa/cyoa.js
+++ Cyoa/cyoa.js
@@ -3,6 +3,15 @@ $(document).ready(function(){
     updateNarrative(N1)
     $("footer").html(createButton("button1", O1) + createButton("button2", O2) + createButton("button3", "not used"));
     $("button3").hide();
+
+    $("button1").click(function(){
+
+        if($("button1").text() === O1){
+            updateNarrative(N2);
+            updateButtons(O4, O5);
+            $("#button3").show();
+        }
+    })
 })
 
 var N1 = "The boy went back to his school after dark to get his..."
@@ -15,3 +24,10 @@ var N7 = ""
 var N8 = ""
 var N9 = ""
 var N10 = ""
+var O1 = "phone"
+var O2 = "wallet"
+var O3 = "charger"
+var O4 = "a demon"
+var O5 = "a ghost"
+var O6 = ""
+var O7 = ""
