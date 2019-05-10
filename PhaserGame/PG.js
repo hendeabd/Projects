d31e777461a0e6103959a7b47655c746b49fda46
unknown <hendeabd000@warren.k12.in.us>
2019-05-10 13:43:34 -0400

    oof

--- PhaserGame/PG.js
+++ PhaserGame/PG.js
@@ -1,11 +1,56 @@
-gameScene.init = function() {
+function cancelEvent(event){
 
+    if(event.preventDefault){
+        event.preventDefault();
+    }
+    else{
+        event.returnValue= false;
+    }
 }
 
-gameScene.preload = function(){
+function cancelPropogation(event){
 
+    if(event.stopPropogation){
+        event.sotpPropogation();
+    }
+    else{
+        event.cancelBubble = true;
+    }
 }
 
-gmaeScene.create = function(){
-    
+function listenEvent(eventTarget,eventType,eventHandler){
+
+    if(eventTarget.addEventListener){
+        eventTarget.addEventListener(eventType,eventHandler,false)
+    }
+    else if(eventTarget.attachEvent){
+        eventType = "on" + eventType;
+        eventTarget.attachEvent(eventType,eventHandler);
+    }
+    else{
+        eventTarget["on" + eventType] = eventHandler;
+    }
+}
+
+//MAP AREA
+
+function level1(i,j){
+    if(
+        (i == 0 && (j >= 0 && j <= 2))
+        || (j == 2 && (i >= 0 && i < 70))
+        ||(i == 70 && (j >= 2 && j <= 28))
+        ||(j == 28 && (j >= 70 && j <= 60))
+        ||(i == 60 && (j >= 28 && j <= 5))
+        ||(j == 5 && (j >= 60 && j <= 40))
+        ||(i == 40 && (j >= 5 && j <= 25))
+        ||(j == 25 && (j >= 40 && j <= 30))
+        ||(i == 30 && (j >= 20 && j <= 25))
+        ||(j == 20 && (j >= 30 && j <= 5))
+        ||(i == 5 && (j >= 20 && j <= 10))
+        ||(j == 10 && (j >= 5 && j <= 80))
+    )
+    {
+        return true;
+    }
+    return false;
 }
